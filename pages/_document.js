ef78df6e942ea45e709987bf821b51beac698be4
steward379 <steward379@gmail.com>
2024-07-10 22:26:56 +0800

    feat: ico favicon added

--- pages/_document.js
+++ pages/_document.js
@@ -32,6 +32,11 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <link rel="icon" href="/favicon/favicon.ico" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+          <link rel="manifest" href="/favicon/site.webmanifest" />
           <link rel="stylesheet" href="/swapgo/tenuki.min.css"></link>
           <script src="/swapgo/tenuki.min.js"></script>
         </Head>
