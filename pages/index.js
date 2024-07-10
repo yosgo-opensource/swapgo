e9459e008939568a16992eea6a7d7a9720143adb
steward379 <steward379@gmail.com>
2024-07-10 14:49:18 +0800

    feat: all logos loaded

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,13 @@ import React from "react";
 
 export default function Home() {
   return (
-    <>Hello World</> 
+    <div>
+      <h1>Hello World</h1>
+      <img src="/logo/swapgo_bg.png" alt="Swago Background" />
+      <img src="/logo/swapgo_trans.png" alt="Swapgo Transparent" />
+      <img src="/logo/swapgo_w.png" alt="Swapgo White" />
+      <img src="/logo/swapgo.png" alt="Swapgo" />
+      <img src="/logo/white.png" alt="White" />
+    </div>
   );
 }
